d3baafed0897f1b7e0147cacb7d76c78e304b79a
Andrew Loebach <Drew@andrews-macbook-pro.local>
2022-07-07 20:29:52 +0000

    Creating functioning minigame converter!

--- minigame converter/convert2minigame.py
+++ minigame converter/convert2minigame.py
@@ -1,65 +1,119 @@
 # This python script will take a directory containing Playdate Lua code as input and convert it into a minigame
 # Author: Andrew Loebach
 
-# NOTE: THIS SCRIPT IS A WORK IN PROGRESS AND IS NOT YET FUNCTIONAL!
-
-### IMPORTING PACKAGES AND SETTING CREDENTIALS ###
-#import json
+# importing packages
 import sys
 import os
 import shutil
-
-#import requests
 import argparse
-
 
 #Parse command-line arguments and set default values if no argument is specified
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="file path where the Playdate game is located")
-parser.add_argument("--git", action="store_true", help="retrieve code from git repository")
-parser.add_argument("--output", help="path where minigame code will be outputted", default="minigame")
+parser.add_argument("path", help="directory where the Playdate game is located")
+parser.add_argument("--output", help="path where minigame code will be outputted")
 parser.add_argument("--name", help="Specify the name of the minigame")
 
-
 args = parser.parse_args()
-# define minigame name
 
-# if the --name option is set, use this as the minigame name
+# define minigame name:
 if args.name:
+	# if the --name option is set, use this as the minigame name
 	minigame_name = args.name.replace(' ','_')
 else:
-	# if no name is provided, extracts the minigame name from the filename
+	# if no name is provided, derives the minigame name from the filename
 	base_name = os.path.basename(args.path)
 	minigame_name = base_name.replace(' ','_')
 
-# for debugging:
-print('minigame name:', minigame_name)
+print('Creating minigame files for:', minigame_name)
+
 	
 # create new directory where we output the minigame code to
 if args.output:
 	output_directory = args.output
 else:
-	output_directory = minigame_name
-	
-if not os.path.isdir(output_directory):
+	output_directory = "_" + minigame_name
+
+print("outputting minigame files to:", output_directory, "..." )
+
+if os.path.isdir(output_directory):
+	user_response = input("Directory " + output_directory + " already exists. Overwrite (y/n)? ")
+	if user_response.lower() != 'y':  
+		print("<aborting script>\n")
+		sys.exit()
+else:
+	# if directory doesn't exist we simply create it
 	os.mkdir(output_directory)
-
-
-'''
-with open(os.path.join(args.output, "main.lua"), "w") as f:
-	f.write(code)
 	
-shutil.copy("pulp.lua", outpath)
-shutil.copy("pulp-audio.lua", outpath)
-'''
 
-print(f"files written to {args.output}")
+# copy game files to output directory
+shutil.copytree(args.path, output_directory, dirs_exist_ok=True)
 
-# load Lua code
+# traverse directory and create a list all .lua files it contains
+file_list = []
+for root, _directories, files in os.walk(output_directory):
+	for file in files:
+		if(file.endswith(".lua")):
+			file_list.append(os.path.join(root,file))
+			
+			
+# create a dictionary i can iterate over and do a replace operation for each element
+function_dictionary = {
+	'playdate.update': minigame_name + '.update',
+	'playdate.cranked': minigame_name + '.cranked',
+	'playdate.crankDocked':minigame_name + '.crankDocked',
+	'playdate.crankUndocked':minigame_name + '.crankUndocked',
+	'playdate.AButtonDown':minigame_name + '.AButtonDown',
+	'playdate.AButtonHeld':minigame_name + '.AButtonHeld',
+	'playdate.AButtonUp':minigame_name + '.AButtonUp',
+	'playdate.BButtonDown':minigame_name + '.BButtonDown',
+	'playdate.BButtonHeld':minigame_name + '.BButtonHeld',
+	'playdate.BButtonUp':minigame_name + '.BButtonUp',
+	'playdate.downButtonDown':minigame_name + '.downButtonDown',
+	'playdate.downButtonUp':minigame_name + '.downButtonUp',
+	'playdate.leftButtonDown':minigame_name + '.leftButtonDown',
+	'playdate.leftButtonUp':minigame_name + '.leftButtonUp',
+	'playdate.rightButtonDown':minigame_name + '.rightButtonDown',
+	'playdate.rightButtonUp':minigame_name + '.rightButtonUp',
+	'playdate.upButtonDown':minigame_name + '.upButtonDown',
+	'playdate.upButtonUp':minigame_name + '.upButtonUp'
+	}
 
-# search and replace standard playdate SDK commands with minigame commands
+# iterate over all lua files
+for filename in file_list:
+	#print("checking file:", filename)
 
-# remove files not relevant for minigame format
-# remove pxinfo
+	with open(filename, "r") as file:
+		data = file.read()
+	
+		# search for standard playdate SDK commands and replace with minigame commands
+		for PD_function in function_dictionary:
+			data = data.replace(PD_function, function_dictionary[PD_function])
+	
+	# Writing the replaced data to file
+	with open(filename, "w") as file:
+		# add minigame name to top of main.lua
+		if os.path.basename(filename) == "main.lua":
+			file.write("-- Define minigame package\n")
+			file.write("local " + minigame_name + " = {}\n")
+			
+		file.write(data) # writing the updated data to file
+		
+		# return minigame package at bottom of main.lua
+		if os.path.basename(filename) == "main.lua":
+			file.write("\n-- Return minigame package\n")
+			file.write("return " + minigame_name)
 
 
+# rename main.lua to the name of the minigame
+try:
+	os.replace(output_directory + '/main.lua', output_directory + '/' + minigame_name + '.lua')
+except:
+	print("ERROR: could not find main.lua")
+
+
+# remove pdxinfo since the minigame doesn't need it
+os.remove(os.path.join(output_directory,"pdxinfo"))
+# remove .pdx file(s)?
+
+print('Minigames files written to', output_directory)
+print('<script completed>\n')
--- minigame converter/readme.txt
+++ minigame converter/readme.txt
@@ -1,1 +1,7 @@
-This minigame converter is not yet functional. Once it is completed it should be able to convert a standard playdate game into the minigame format for use with Mobware Minigames
+This minigame converter will allow you to take a playdate game written in in Lua and convert it into a minigame format for use with Mobware Minigames. 
+
+Usage: python3 convert2minigame.py <path> --name <minigame name> [--output <output directory>]
+
+example: The following command will take the playdate code found in the directory "Asheteroids", create a minigame named "asheteroids"
+
+python3 convert2minigame.py Asheteroids --name asheteroids
